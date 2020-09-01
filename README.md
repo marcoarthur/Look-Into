14bc3cc70e00fffa3db4996e27d56f8441b2b988
Marco Arthur <arthurpbs@gmail.com>
2020-08-31 23:47:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Look::Into - Blah blah blah
+
+# SYNOPSIS
+
+    use Look::Into;
+
+# DESCRIPTION
+
+Look::Into is
+
+# AUTHOR
+
+Marco Arthur <arthurpbs@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2020- Marco Arthur
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Look/Into.pm
@@ -0,0 +1,39 @@
+package Look::Into;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Look::Into - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Look::Into;
+
+=head1 DESCRIPTION
+
+Look::Into is
+
+=head1 AUTHOR
+
+Marco Arthur E<lt>arthurpbs@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2020- Marco Arthur
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
